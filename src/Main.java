bff30d3ff73058f22748f8441b5fac93af4fa684
Jhan Carlos <vasquezl.jc@gmail.com>
2022-08-20 01:02:49 -0500

    Funcion corregido,quitar brazos

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        suma(7, 2);
-        //Este comentario
+        Potato miPotato = new Potato();
+        miPotato.QuitarBrazo(); //menos 1 brazo
+        miPotato.QuitarBrazo(); //menos 1 brazo
+        miPotato.QuitarBrazo(); //menos 1 brazo
+        System.out.println(miPotato.brazos);
+
     }
-    public static void suma(int a, int b){
-        int resultado;
-        resultado = a +b;
-        System.out.println(resultado);
+
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
+
+class Potato{
+    public int brazos = 0;
+
+    public void QuitarBrazo(){
+        this.brazos--;
     }
 }
