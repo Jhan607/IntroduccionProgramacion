03de48e47381b1b5ef9f8e0b2e63ef7e063ac4c5
Jhan Carlos <vasquezl.jc@gmail.com>
2022-08-20 00:45:27 -0500

    Funciones 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(7, 2);
+        //Este comentario
+    }
+    public static void suma(int a, int b){
+        int resultado;
+        resultado = a +b;
+        System.out.println(resultado);
+    }
+}
